55fa8d897be66345f0104449a12fe8da43ea3e78
zhanglaiya <zhanglaiya@4kmiles.com>
2023-01-11 10:01:35 +0800

    require sqlalchemy

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@
 from setuptools import setup
 setup(
     name='webdevtool',
-    install_requires=['fastapi', 'paramiko', 'websockets']
+    install_requires=['fastapi', 'paramiko', 'websockets', 'sqlalchemy']
 )
