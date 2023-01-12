3b8a8d9ffaf0ae258eea13d80df6d65a85313989
zhanglaiya <zhanglaiya@4kmiles.com>
2023-01-12 18:34:15 +0800

    require cryptography

---
+++ src/webdevtool/crypto.py
@@ -0,0 +1,7 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, PublicFormat, NoEncryption
+
+
+class RSA:
+
+    def __init__(self):
