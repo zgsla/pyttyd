5c7ae6de1d5b611764f5e770b4c2ef466b5f7b14
zhanglaiya <zhanglaiya@4kmiles.com>
2023-01-12 18:34:15 +0800

    require cryptography

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@
 from setuptools import setup
 setup(
     name='webdevtool',
-    install_requires=['fastapi', 'paramiko', 'websockets', 'sqlalchemy']
+    install_requires=['cryptography', 'fastapi', 'paramiko', 'websockets', 'sqlalchemy']
 )
