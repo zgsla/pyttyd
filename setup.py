008d71cef77ec1b512f8999ccfe977a0d7ea915a
zhanglaiya <zhanglaiya@4kmiles.com>
2023-02-15 09:49:18 +0800

    fix

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@
 from setuptools import setup
 setup(
     name='pyttyd',
-    install_requires=['cryptography', 'fastapi', 'uvicorn', 'paramiko', 'websockets', 'sqlalchemy', 'jinja2']
+    install_requires=['cryptography', 'fastapi', 'uvicorn', 'paramiko', 'websockets', 'sqlalchemy', 'jinja2'],
+    include_package_data=True,  # 包含静态文件
 )
