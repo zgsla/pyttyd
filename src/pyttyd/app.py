84b050b342581880b284139e2cf7d375a19dd2ef
张来亚 <zhangly@vzoom.com>
2024-03-22 12:00:30 +0800

    pty

--- src/pyttyd/app.py
+++ src/pyttyd/app.py
@@ -1,13 +1,9 @@
-import pty
-import asyncio
-import subprocess
-
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
 from pyttyd import __static__, __html__
-from pyttyd.terminal import read_sock, read_chan
+from pyttyd.pty import PTY
 
 app = FastAPI()
 
@@ -27,14 +23,5 @@ async def websocket_endpoint(
 ):
     await websocket.accept()
     print('accepted')
-    master, slave = pty.openpty()
-    process = subprocess.Popen('bash', cwd='~', stdin=slave, stdout=slave, stderr=slave, shell=True)
-    consumer_task = asyncio.create_task(read_chan(websocket, master))
-    producer_task = asyncio.create_task(read_sock(websocket, master))
-
-    done, pending = await asyncio.wait(
-        [consumer_task, producer_task],
-        return_when=asyncio.FIRST_COMPLETED,
-    )
-    for task in pending:
-        task.cancel()
+    async with PTY(websocket) as pty:
+        await pty.process()
