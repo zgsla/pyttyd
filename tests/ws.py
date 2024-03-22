d9e60ad7430a0e3e70919ca68eb54fc2e4cde696
张来亚 <zhangly@vzoom.com>
2024-03-22 14:28:53 +0800

    fix test

--- tests/ws.py
+++ tests/ws.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from websockets.sync.client import connect
 
@@ -5,11 +6,12 @@ from websockets.sync.client import connect
 class TestTTY(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.websocket = connect('127.0.0.1:8221')
+        self.websocket = connect('ws://127.0.0.1:8221/tty?rows=100&cols=100')
 
     def test_send(self):
-        self.websocket.send('echo "hello world"\n')
+        self.websocket.send(json.dumps({'input': 'echo "hello world"\n'}))
         data = self.websocket.recv(10)
+        print(data)
         assert data == 'hello world'
 
     def tearDown(self) -> None:
